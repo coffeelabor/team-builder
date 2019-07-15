003025b7b1e77c47039e1038ec43484f59033b7d
Reed James <reedtjames1234@gmail.com>
2019-07-15 18:31:39 -0500

    Commit before standup, working on add and edit functionality

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,17 +1,38 @@
 import React, { useState, useEffect } from "react";
+import Form from "./components/Form";
 
 import "./App.css";
 
 function App() {
-  const [teamState, setTeamState] = useState({
-    squirrel: "",
-    owl: "",
-    mole: ""
-  });
-  // console.log("teamState", teamState);
+  const [teamState, setTeamState] = useState([
+    {
+      name: "",
+      email: "",
+      role: ""
+    }
+  ]);
+  console.log("teamState", teamState);
+
+  const submitHandler = e => {
+    e.preventDefault();
+    setTeamState();
+  };
+
+  const changeHandler = e => {
+    setTeamState({
+      ...teamState,
+      [e.target.name]: e.target.value
+    });
+  };
+
   return (
     <div className="App">
       <h1>hello</h1>
+      <Form
+        teamData={teamState}
+        submitHandler={submitHandler}
+        changeHandler={changeHandler}
+      />
     </div>
   );
 }
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,9 +1,39 @@
 import React from "react";
 
-const Form = () => {
+const Form = props => {
+  console.log("form props", props);
   return (
     <div>
-      <h1>howdy</h1>
+      <form onSubmit={props.submitHandler}>
+        <label>
+          Name:
+          <input
+            name="name"
+            type="text"
+            placeholder="name"
+            onChange={props.changeHandler}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            name="email"
+            type="text"
+            placeholder="email"
+            onChange={props.changeHandler}
+          />
+        </label>
+        <label>
+          Role:
+          <input
+            name="role"
+            type="text"
+            placeholder="role"
+            onChange={props.changeHandler}
+          />
+        </label>
+        <button>push it</button>
+      </form>
     </div>
   );
 };
